25d3415ecb86b901d9f3be2c6cce660a197bb6b0
Sebastian Zevallos <zevallosmc06@gmail.com>
2026-08-15 21:17:05 -0500

    Se modifica Hola Mundo

--- Downloads/Proyectos/HolaMundo.java
+++ Downloads/Proyectos/HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
     public void main(String Args[]) {
         System.out.println("Hola Mundo");
         System.out.println("Juan Sebastian");
+        System.out.println("UTP");
 
     }
 }
